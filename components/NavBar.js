925e3e0ccaea895c3b7132583c9d78a62cb813b2
TuckerLandis <landistuckerc@gmail.com>
2023-02-08 00:31:55 -0600

    init

---
+++ components/NavBar.js
@@ -0,0 +1,67 @@
+// import Pencil from './pencil.svg'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+
+export default function NavBar() {
+    return (
+        <>
+            <div className={styles.nav}>
+
+                <div className={styles.navItem}>
+
+                    <Image
+                        src="/musicnoteBit.svg"
+                        alt="PenPlotter"
+                        // className={styles.vercelLogo}
+                        width={30}
+                        height={30}
+                    // priority
+                    />
+                    <p className={styles.navItemText}>music</p>
+                </div>
+
+                <div className={styles.navItem}>
+                    <Image
+                        src="/pencilBit.svg"
+                        alt="PenPlotter"
+                        // className={styles.vercelLogo}
+                        width={30}
+                        height={30}
+                        priority
+                    />
+                    <p className={styles.navItemText}>plots</p>
+                </div>
+
+                <div className={styles.navItem}>
+
+                    <Image
+                        src="/monitorBit.svg"
+                        alt="PenPlotter"
+                        // className={styles.vercelLogo}
+                        width={30}
+                        height={30}
+                    // priority
+                    />
+                    <p className={styles.navItemText}>video</p>
+                </div>
+
+
+
+                {/* <div className={styles.navItem}>
+
+                    <Image
+                        src="/lightbulbBit.svg"
+                        alt="PenPlotter"
+                        // className={styles.vercelLogo}
+                        width={30}
+                        height={30}
+                    // priority
+                    />
+                    <p className={styles.navItemText}>col</p>
+                </div> */}
+
+
+            </div>
+        </>
+    )
+}
